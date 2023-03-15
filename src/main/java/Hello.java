968f6e2505f82765d22959c2704ace7ca313bafb
Kimsojeong <sssoj0612@gmail.com>
2023-03-15 09:39:29 +0900

    소스 수정2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("안녕~!");
-        System.out.println("집");
+        System.out.println("김시루 보고싶다");
     }
 }
