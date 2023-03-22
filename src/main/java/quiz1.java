dd1b701297ca378e605a3094f57b34379a034798
Kimsojeong <sssoj0612@gmail.com>
2023-03-22 11:51:53 +0900

    [AI활용및코딩] 과제1

---
+++ src/main/java/quiz1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class quiz1 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a;
+
+        System.out.printf("정수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (a%3==0) {
+            System.out.printf("3의 배수입니다.\n");
+        } else{
+            System.out.printf("3의 배수가 아닙니다.\n");
+        }
+    }
+}
