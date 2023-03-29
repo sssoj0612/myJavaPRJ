496db3cfbb594603622db2be7351a2fa560774b6
Kimsojeong <sssoj0612@gmail.com>
2023-03-29 14:05:07 +0900

    [AI활용및코딩] 과제3

---
+++ src/main/java/quiz3.java
@@ -0,0 +1,12 @@
+public class quiz3 {
+    public static void main(String[]args){
+        int i,k;
+
+        for (i=1 ; i<10 ; i++) {
+            for(k=2; k<10 ; k++) {
+                System.out.printf("%d * %d = %2d     ", k,i,k*i);
+            }
+            System.out.printf("\n");
+            }
+        }
+    }
