a00756a981aa284d05772151686ae55d979bc446
Kimsojeong <sssoj0612@gmail.com>
2023-04-10 17:56:05 +0900

    2023.04.10 시험 점수를 입력받아 점수에 따라 A~F 출력하기

--- src/main/java/Baekjoon_9498.java
+++ src/main/java/Baekjoon_9498.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 public class Baekjoon_9498 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int a,b;
+        int score; //시험 점수
 
         Scanner s = new Scanner(System.in);
+        score = s.nextInt(); //시험 점수는 입력 받는다.
 
-        System.out.printf("첫 번째 숫자를 입력하시오 : ");
-        a = s.nextInt();
-
-        System.out.printf("두 번째 숫자를 입력하시오 : ");
-        b = s.nextInt();
-
-        System.out.println();
-
-        if (a>b)
-            System.out.printf("크기 비교 : %d > %d \n", a, b);
-
-             else if (a<b)
-                System.out.printf("크기 비교 : %d < %d \n", a, b);
-
-                else if (a==b)
-                    System.out.printf("크기 비교 : %d == %d \n", a, b);
-        }
+            if (score > 89) {
+                System.out.println("A"); // 90 ~ 100점은 A
+            } else if (score > 79) {
+                System.out.println("B"); // 80 ~ 89점은 B
+            } else if (score > 69) {
+                System.out.println("C"); // 70 ~ 79점은 C
+            } else if (score > 59) {
+            System.out.println("D"); // 60 ~ 69점은 D
+                } else
+                System.out.println("F"); // 그 이하는 F
     }
+}
