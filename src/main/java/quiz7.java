636f319b9b41cb9e371af8ba534fe9a316c2f23e
Kimsojeong <sssoj0612@gmail.com>
2023-04-13 10:48:56 +0900

    [AI활용및코딩] 퀴즈7 2023.04.13

--- src/main/java/quiz7.java
+++ src/main/java/quiz7.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-
 public class quiz7 {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
+
         int nums[] = new int[5]; // 숫자를 저장할 배열
         int sum = 0; //평균을 구하기 위한 합계
 
         for (int i = 0; i < nums.length; i++) { // 숫자 입력을 위한 for문
             System.out.printf("%d번째 값 : ", i + 1);
             nums[i] = s.nextInt();
-            sum += nums[i]; // 평균을 구하기 위해 입력 된 값들의 합을 저장
+            sum += nums[i]; // 평균값 구하기 위해 입력 된 값들의 합을 저장
         }
 
         int max = nums[0]; //최대값을 배열 1번째 값으로 초기화
@@ -18,7 +18,7 @@ public class quiz7 {
 
         for (int i = 0; i < nums.length; i++) { //최대값,최소값을 구하기위한 for문
             if (nums[i] > max) { //배열의 i번째 값과 max에 저장 된 값을 비교
-                max = nums[i];
+                max = nums[i]; //배열의 i번째 값이 크면 max 값 변경
                 }
 
             else if (nums[i] < min) {
