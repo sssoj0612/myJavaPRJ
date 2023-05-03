d144d43e02ea8bdbac4e584c793f5a758059b41a
chlwnstkd <2320110213@office.kopo.ac.kr>
2023-05-03 10:09:24 +0900

    문자열의 길이를 알려주는 length() 메소드

---
+++ src/main/java/Ex09_01.java
@@ -0,0 +1,11 @@
+public class Ex09_01 {
+    public static void main(String[] args) {
+        String str = "IT CookBook. Java";
+        int len;
+
+        len = str.length();
+
+        System.out.printf("문자열 : %s \n", str);
+        System.out.printf("문자열 길이: %d \n", len);
+    }
+}
