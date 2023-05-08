2bc39048f3e73076e85445355934fedeff28129b
Kimsojeong <sssoj0612@gmail.com>
2023-05-08 22:10:29 +0900

    23.05.08 자바스터디

--- src/main/java/Study_230505.java
+++ src/main/java/Study_230505.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Study_230505 {
-    public static void main(String[] args) {
+    public static void record(String yes) {
 
         Scanner s = new Scanner(System.in);
 
@@ -10,34 +10,56 @@ public class Study_230505 {
         // List 구조 안에 저장될 Map 객체
         Map<String, String> map = null;
 
-            System.out.print("이름 : ");
-            String name = s.next(); // 이름 입력받기
+            System.out.print("날짜 : "); // 값 입력 받기
+            String date = s.next();
 
-            System.out.print("날짜 : ");
-            String date = s.next(); // 날짜 입력받기
+            System.out.print("아침 : ");
+            String brekker = s.next();
 
-            System.out.print("운동 : ");
-            String sport = s.next(); // 할 운동 입력받기
+            System.out.print("점심 : ");
+            String launch = s.next();
+
+            System.out.print("저녁 : ");
+            String dinner = s.next();
+
+            System.out.print("몸무게 : ");
+            String weight = s.next();
 
             System.out.println();
 
             map = new HashMap<>(); // List 구조에 저장할 Map 메모리에 올리기
 
-            map.put("name", name); // 입력받은 값을 name 키에 저장
-            map.put("date", date); // 입력받은 값을 date 키에 저장
-            map.put("sport", sport); // 입력받은 값을 sport 키에 저장
+            map.put("date", date); // 입력받은 값을 각각 키에 저장
+            map.put("brekker", brekker);
+            map.put("launch", launch);
+            map.put("dinner", dinner);
+            map.put("weight", weight);
 
             list.add(map); // Map 구조를 List에 저장하기
             map = null; // 다 사용하면 메모리 비우기
 
-        list.forEach(rMap -> { // 입력한 값 출력
-            System.out.println("name : " + rMap.get("name"));
-            System.out.println("date : " + rMap.get("date"));
-            System.out.println("sport : " + rMap.get("sport"));
-            System.out.println();
-        } );
+            list.forEach(rMap -> { // 입력한 값 출력
+                System.out.println("date : " + rMap.get("date"));
+                System.out.println("brekker : " + rMap.get("brekker"));
+                System.out.println("launch : " + rMap.get("launch"));
+                System.out.println("dinner : " + rMap.get("dinner"));
+                System.out.println("weight : " + rMap.get("weight"));
+                System.out.println("- 정상적으로 기록되었습니다. -");
+                System.out.println();
+            });
+        }
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("식단 기록을 하시겠습니까? <1>yes <2>no");
+        int a = s.nextInt();
+
+        if (a==1){ // record 메소드 호출
+            record("yes");
+        }
+
+        else
+            System.out.print("종료합니다.");
     }
-
-
-
 }
